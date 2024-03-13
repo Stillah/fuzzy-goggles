e5582e2f3f7950af3061c25ec3f5cfe0d05a3174
Ruslan <150805779+Stillah@users.noreply.github.com>
2024-03-13 13:35:05 +0300

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int partition(int arr[], int low, int high) {
-  int pivot = arr[high];
-  int left = low;
-  for (int i = low; i < high; i++) {
-    if (arr[i] <= pivot) swap(arr[i], arr[left]), left++;
-  }
-  swap(arr[high], arr[left]);
-  return left;
+template <typename T>
+int partition(vector<T>& arr, int low, int high) {
+    int pivot = arr[high];
+    int left = low;
+    for (int i = low; i < high; i++) {
+        if (arr[i] <= pivot) swap(arr[i], arr[left]), left++;
+    }
+    swap(arr[high], arr[left]);
+    return left;
 }
  
-void quickSort(int arr[], int low, int high) {
-  if (low < high) {
-    int pivot_location = partition(arr, low, high);
-    quickSort(arr, low, pivot_location - 1);
-    quickSort(arr, pivot_location + 1, high);
-  }
+template <typename T>
+void quickSort(vector<T>& arr, int low, int high) {
+    if (low < high) {
+        int pivot_location = partition(arr, low, high);
+        quickSort(arr, low, pivot_location - 1);
+        quickSort(arr, pivot_location + 1, high);
+    }
 }
