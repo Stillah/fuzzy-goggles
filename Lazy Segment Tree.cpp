e06140ccf41ef7d6fd16a85f6a0c5643b37bd057
Ruslan <150805779+Stillah@users.noreply.github.com>
2024-05-22 12:16:05 +0300

    made tree generic

--- Lazy Segment Tree.cpp
+++ Lazy Segment Tree.cpp
@@ -3,47 +3,51 @@
 using namespace std;
 typedef long long ll;
 
-struct Node {
-    ll sum;
+template <typename T>
+class Node {
+public:
+    T sum;
     bool isAssigned;
-    ll assigned;
-    Node(ll sum, bool isAssigned, ll assigned): sum(sum), isAssigned(isAssigned), assigned(assigned) {};
+    T assigned;
+    Node(T sum, bool isAssigned, T assigned): sum(sum), isAssigned(isAssigned), assigned(assigned) {};
 };
 
-//To change from sum segment tree to other segment tree, change func(ll a, ll b), assignOnNode(int v, int vl, int vr, ll x)
+//To change from sum segment tree to other segment tree, change func(T a, T b), assignOnNode(int v, int vl, int vr, T x)
 //and identity
-struct SegmentTree {
-    vector<Node> tree;
-    ll sz;
-    ll identity = 0;
-    
-    explicit SegmentTree(vector<ll>& a) {
+template <typename T>
+class SegmentTree {
+public:
+    vector<Node<T>> tree;
+    int sz;
+    T identity = 0;
+
+    explicit SegmentTree(vector<T>& a) {
         int n = 1;
         while (n < a.size()) n *= 2;
 
-        tree.assign(2*n, Node(identity, false, 0));
+        tree.assign(2*n, Node<T>(identity, false, 0));
         sz = tree.size() / 2;
-        
+
         for (int i = 0; i < a.size(); i++)
             tree[n+i].sum = a[i];
 
         for (int i = n-1; i > 0; i--)
             recalculate(i);
     }
-    
+
     explicit SegmentTree(int size) {
         int n = 1;
         while (n < size) n *= 2;
-        
+
         tree.assign(2*n, Node(identity, false, 0));
         sz = tree.size() / 2;
     }
 
     void recalculate(int v) {
-        tree[v].sum = func(tree[2*v].sum, tree[2*v + 1].sum);
+        tree[v].sum = tree[2*v].sum + tree[2*v + 1].sum;
     }
 
-    ll action(int v, int vl, int vr, int ql, int qr) {
+    T action(int v, int vl, int vr, int ql, int qr) {
         if (vr < ql || qr < vl) return identity;
         if (ql <= vl && vr <= qr) return tree[v].sum;
         push(v,vl,vr);
@@ -51,7 +55,7 @@ struct SegmentTree {
         return func(action(2*v, vl, vmid, ql, qr), action(2*v + 1, vmid + 1, vr, ql, qr));
     }
 
-    ll action (int ql, int qr) {
+    T action (int ql, int qr) {
         return action(1, 1, sz, ql, qr);
     }
 
@@ -65,17 +69,17 @@ struct SegmentTree {
         }
     }
 
-    void assignOnNode(int v, int vl, int vr, ll x) {
+    void assignOnNode(int v, int vl, int vr, T x) {
         //For min/max to add a value
-        tree[v] = {tree[v].sum + x, true, tree[v].assigned + x};
-        
+        //tree[v] = {tree[v].sum + x, true, tree[v].assigned + x};
+
         //For sum to add a value
-        //tree[v] = {tree[v].sum + x * (vr - vl + 1), true, tree[v].assigned + x};
-        
+        tree[v] = {tree[v].sum + x * (vr - vl + 1), true, tree[v].assigned + x};
+
         //To set a value remove "tree[v].sum +", "tree[v].assigned +"
     }
 
-    void assignOnSegment(int v, int vl, int vr, int ql, int qr, ll x) {
+    void assignOnSegment(int v, int vl, int vr, int ql, int qr, T x) {
         if (vr < ql || qr < vl) return;
         if (ql <= vl && vr <= qr) {
             assignOnNode(v,vl,vr,x);
@@ -88,15 +92,15 @@ struct SegmentTree {
         recalculate(v);
     }
 
-    void assignOnSegment(int ql, int qr, ll x) {
+    void assignOnSegment(int ql, int qr, T x) {
         assignOnSegment(1, 1, sz, ql, qr, x);
     }
-    
-    void assignOnSingleNode(int q, ll x) {
+
+    void assignOnSingleNode(int q, T x) {
         assignOnSegment(1, 1, sz, q, q, x);
     }
 
-    ll func(ll a, ll b) {
+    T func(T a, T b) {
         return a + b;
     }
 };
